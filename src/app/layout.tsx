7294d7578d38a5d1db22751e7c51fd29554b16e6
Brian Whaley <brian.whaley@gmail.com>
2025-12-22 10:52:50 -0500

    refactor fonts, export google font preconnect, clean up localbusiness visualdesign and generatemetatags on layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,46 +33,13 @@ export default async function RootLayout({children}: Readonly<{children: React.R
 					title: metadata?.title ?? "",
 					description: metadata?.description ?? "",
 					keywords: metadata?.keywords ?? "",
-					site_name: "Brian Whaley",
-					email: "brian.whaley@gmail.com",
 					origin: origin ?? "",
-					url: url ?? "",
-					image: "/images/pix/pix-bg-bw-big.gif",
-					image_height: "512",
-					image_width: "512",
-					favicon: "/images/favicon.ico"
+					url: url ?? ""
 				}) }
-				<LocalBusinessSchema
-					name="Brian Whaley"
-					streetAddress="10 Jade Circle"
-					addressLocality="Denville"
-					addressRegion="NJ"
-					postalCode="07834"
-					addressCountry="US"
-					telephone="+1-973-710-8008"
-					url="https://brianwhaley.com"
-					email="brian.whaley@gmail.com"
-					image="/images/pix/pix-bg-bw-big.gif"
-					priceRange="$"
-					description="Professional web developer and technology consultant specializing in custom web development, mobile applications, and digital products."
-					sameAs={[
-						"https://www.linkedin.com/in/brianwhaley",
-						"http://www.facebook.com/brian.t.whaley",
-						"http://twitter.com/brianwhaley",
-						"http://www.youtube.com/user/brianwhaley",
-						"http://www.pinterest.com/brianwhaley",
-						"http://reddit.com/user/btw-73",
-						"https://maps.app.goo.gl/j5Tpcxxr9roydxd2A",
-						"http://pixelatedviews.tumblr.com",
-						"https://www.etsy.com/people/bwhaley73",
-						"http://trees.ancestry.com/tree/7237865",
-						"https://github.com/brianwhaley",
-						"https://www.goodreads.com/user/show/49377228-brian-whaley"
-					]}
-				/>
+				<LocalBusinessSchema siteInfo={myRoutes.siteInfo} />
+				<VisualDesignStyles visualdesign={myRoutes.visualdesign} />
 				<meta name="google-site-verification" content="t2yy9wL1bXPiPQjBqDee2BTgpiGQjwVldlfa4X5CQkU" />
 				<meta name="google-site-verification" content="l7D0Y_JsgtACBKNCeFAXPe-UWqo13fPTUCWhkmHStZ4" />
-				<VisualDesignStyles visualdesign={myRoutes.visualdesign} />
 			</head>
 			<body>
 				<PixelatedServerConfigProvider>
