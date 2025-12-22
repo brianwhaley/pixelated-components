c54b8df01ac86482e9bf6972d617b9833bf0bd63
Brian Whaley <brian.whaley@gmail.com>
2025-12-22 10:53:01 -0500

    refactor fonts, export google font preconnect, clean up localbusiness visualdesign and generatemetatags on layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-page-custom-font */
 import { headers } from 'next/headers';
 import { getRouteByKey } from '@pixelated-tech/components/server';
 import { generateMetaTags } from "@pixelated-tech/components/server";
@@ -40,40 +39,13 @@ export default async function RootLayout({
 					title: metadata?.title ?? "",
 					description: metadata?.description ?? "",
 					keywords: metadata?.keywords ?? "",
-					site_name: "Oaktree Landscaping",
-					email: "oaktreelandscaper@gmail.com",
 					origin: origin ?? "",
-					url: url ?? "",
-					image: "/images/pix/pix-bg-512.gif",
-					image_height: "512",
-					image_width: "512",
-					favicon: "/images/favicon.ico"
+					url: url ?? ""
 				}) }
-				<LocalBusinessSchema
-					name="Oaktree Landscaping"
-					streetAddress=""
-					addressLocality="Bluffton"
-					addressRegion="SC"
-					postalCode="29909"
-					addressCountry="US"
-					telephone="+1-843-707-6481"
-					url="https://oaktreelandscaping.com"
-					email="oaktreelandscaper@gmail.com"
-					image="/images/logo/oaktree-logo-vertical.jpg"
-					priceRange="$"
-					description="Professional landscaping and outdoor design services for residential and commercial properties."
-					sameAs={[
-						"https://www.facebook.com/oaktreelandscaping",
-						"https://www.instagram.com/oaktreelandscaping"
-					]}
-				/>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" />
-				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" />
+				<LocalBusinessSchema siteInfo={myRoutes.siteInfo} />
+				<VisualDesignStyles visualdesign={myRoutes.visualdesign} />
 			</head>
 			<body>
-				<VisualDesignStyles visualdesign={myRoutes.visualdesign} />
 				<PixelatedServerConfigProvider>
 					<header>
 						<Header />
