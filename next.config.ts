4163a062c3458cfb6f463a21f73109abd7852a3f
Brian Whaley <brian.whaley@gmail.com>
2025-12-26 16:35:07 -0500

    bump component version, add google analytics tag to footer

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,9 @@ const nextConfig: NextConfig = {
 		return [];
 	},
 
-	turbopack: {},
+	turbopack: {
+		root: __dirname,
+	},
 	webpack: (config) => {
 		config.resolve.fallback = { 
 			fs: false,
--- src/app/elements/footer.jsx
+++ src/app/elements/footer.jsx
@@ -1,14 +1,16 @@
 import React, { } from 'react';
+import { GoogleAnalytics } from "@pixelated-tech/components";
 
 export default function Footer() {
-  return (
-    <>
-    	<hr style={{ margin: "0 auto", width: "80%" }} />
-      <br />
-      <div className="centered">
-        <p className="footer-text">&copy; {new Date().getFullYear()} InformationFocus. All rights reserved.</p>
-      </div>
-      <br /><br />
-    </>
-  );
+	return (
+    	<>
+			<GoogleAnalytics id="G-0FNGDMEKMS" />
+    		<hr style={{ margin: "0 auto", width: "80%" }} />
+      		<br />
+      		<div className="centered">
+        		<p className="footer-text">&copy; {new Date().getFullYear()} InformationFocus. All rights reserved.</p>
+      		</div>
+      		<br /><br />
+    	</>
+  	);
 }
